8922a94cbf8cdb66855b93573b2da80e67a857f6
Todd Kjos <tkjos@google.com>
2015-10-14 16:25:48 -0700

    Revert "[ALPS01783144] sched: fix load unbalance when CPU idle"
    This reverts commit f413a273fa0ca86063b1858ee148b64a292473f6.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -8098,7 +8098,6 @@ static struct sched_group *find_busiest_group(struct lb_env *env)
 
 #ifdef CONFIG_MT_LOAD_BALANCE_ENHANCEMENT
 	if ((env->idle == CPU_IDLE) || (env->idle == CPU_NEWLY_IDLE)) {
-		int i = (env->idle == CPU_IDLE) ? 1:0;
 #else
 	if (env->idle == CPU_IDLE) {
 #endif
@@ -8109,18 +8108,9 @@ static struct sched_group *find_busiest_group(struct lb_env *env)
 		 * significant if the diff is greater than 1 otherwise we
 		 * might end up to just move the imbalance on another group
 		 */
-#ifdef CONFIG_MT_LOAD_BALANCE_ENHANCEMENT
 		if ((busiest->group_type != group_overloaded) &&
-				(local->idle_cpus < (busiest->idle_cpus + i))) {
-#else
-		if ((busiest->group_type != group_overloaded) &&
-				(local->idle_cpus <= (busiest->idle_cpus + 1))) {
-#endif
-
-			mt_sched_printf(sched_lb, "[%s] fail b_type=%d s_idles=%d b_idles=%d",
-				__func__, busiest->group_type, local->idle_cpus, busiest->idle_cpus);
+				(local->idle_cpus <= (busiest->idle_cpus + 1)))
 			goto out_balanced;
-		}
 	} else {
 		/*
 		 * In the CPU_NEWLY_IDLE, CPU_NOT_IDLE cases, use
