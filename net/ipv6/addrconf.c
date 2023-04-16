ea313ddd12e8b88204fccd68e5d46d4d91310d82
Johannes Berg <johannes.berg@intel.com>
2023-04-16 06:41:17 +0000

    ipv6: add option to drop unicast encapsulated in L2 multicast
    In order to solve a problem with 802.11, the so-called hole-196 attack,
    add an option (sysctl) called "drop_unicast_in_l2_multicast" which, if
    enabled, causes the stack to drop IPv6 unicast packets encapsulated in
    link-layer multi- or broadcast frames. Such frames can (as an attack)
    be created by any member of the same wireless network and transmitted
    as valid encrypted frames since the symmetric key for broadcast frames
    is shared between all stations.
    
    Reviewed-by: Julian Anastasov <ja@ssi.bg>
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit dede82143bf1bbf92ea73a519bb0298b19c56cb9)
    
    Change-Id: I76c8f84b53e95c40ad3c2b5adac0ec4964cc920c

--- net/ipv6/addrconf.c
+++ net/ipv6/addrconf.c
@@ -4421,6 +4421,7 @@ static inline void ipv6_store_devconf(struct ipv6_devconf *cnf,
 #endif
 	array[DEVCONF_SUPPRESS_FRAG_NDISC] = cnf->suppress_frag_ndisc;
 	array[DEVCONF_ACCEPT_RA_FROM_LOCAL] = cnf->accept_ra_from_local;
+	array[DEVCONF_DROP_UNICAST_IN_L2_MULTICAST] = cnf->drop_unicast_in_l2_multicast;
 }
 
 static inline size_t inet6_ifla6_size(void)
@@ -5379,6 +5380,13 @@ static struct addrconf_sysctl_table
 			.proc_handler	= proc_dointvec
 		},
 #endif
+		{
+			.procname	= "drop_unicast_in_l2_multicast",
+			.data		= &ipv6_devconf.drop_unicast_in_l2_multicast,
+			.maxlen		= sizeof(int),
+			.mode		= 0644,
+			.proc_handler	= proc_dointvec,
+		},
 		{
 			/* sentinel */
 		}
