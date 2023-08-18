aosp-mirror/kernel_msm

binder: Add instramentation to dump_ref_desc_tree()
b/110201845 and b/30870027 has been seen a few times since code
was added to dump out the ref_desc_tree when we hit the
error condition which appears to be impossible.

In those cases, the rb tree appeared to be correct, so this
didn't provide enough information to diagnose the issue. This
patch adds the missing piece of data that hopefully will indicate
why we break out of the desc assignment loop when we have not
yet found a free desc.

There are two conditions that break out of the loop:

  n==NULL or (ref->data.desc > new_ref->data.desc)

both appear to be impossible, but one of them must
have occurred. Adds "n" and "ref->data.desc" to the
output.

Bug: 110201845
Bug: 30870027
Change-Id: I765060e87111cf747e12b1e51aedfc3c770e44bc
Signed-off-by: Todd Kjos <tkjos@google.com>

drivers/android/binder.c
@@ -1637,18 +1637,27 @@ static struct binder_ref *binder_get_ref_olocked(struct binder_proc *proc,
 	return NULL;
 }
 
-static void dump_ref_desc_tree(struct binder_ref *new_ref)
+
+static void dump_ref_desc_tree(struct binder_ref *new_ref, struct rb_node *n_in)
 {
 	struct binder_proc *proc = new_ref->proc;
 	uint32_t desc = new_ref->data.desc;
 	struct rb_node *n, *p;
+	struct binder_ref *ref;
 	int i = 0;
 
-	pr_info("BUG.%d:%d: dump of refs_by_desc rb tree, new desc=%u\n",
-			proc->pid, current->pid, desc);
+	pr_info("BUG.%d:%d: dump of refs_by_desc rb tree, new desc=%u, n%sNULL\n",
+			proc->pid, current->pid, desc, n_in ? "!=" : "==");
+	if (n_in) {
+		ref = rb_entry(n_in, struct binder_ref, rb_node_desc);
+		pr_info("ref containing n: id %d desc %u n %d s %d w %d\n",
+			ref->data.debug_id, ref->data.desc,
+			ref->node->debug_id,
+			ref->data.strong, ref->data.weak);
+	}
+
 	for (n = rb_first(&proc->refs_by_desc); n != NULL; n = rb_next(n)) {
 		struct binder_ref *left=NULL, *right=NULL, *parent=NULL;
-		struct binder_ref *ref;
 
 		ref = rb_entry(n, struct binder_ref, rb_node_desc);
 		if (n->rb_left)
@@ -1661,10 +1670,11 @@ static void dump_ref_desc_tree(struct binder_ref *new_ref)
 		if (p)
 			parent = rb_entry(p, struct binder_ref, rb_node_desc);
 
-		pr_info("%d: id %d desc %u%s%s n %d s %d w %d l %d r %d p %d\n",
+		pr_info("%d: id %d desc %u%s%s%s n %d s %d w %d l %d r %d p %d\n",
 			i++, ref->data.debug_id, ref->data.desc,
 			(n == proc->refs_by_desc.rb_node) ? " (ROOT)" : "",
 			(ref->data.desc == desc) ? " (MATCH)" : "",
+			(n_in == n) ? " (BREAK)" : "",
 			ref->node->debug_id,
 			ref->data.strong, ref->data.weak,
 			left ? left->data.debug_id : -1,
@@ -1741,7 +1751,7 @@ static struct binder_ref *binder_get_ref_for_node_olocked(
 		else if (new_ref->data.desc > ref->data.desc)
 			p = &(*p)->rb_right;
 		else {
-			dump_ref_desc_tree(new_ref);
+			dump_ref_desc_tree(new_ref, n);
 			BUG();
 		}
 	}

almondnguyen/android_kernel_samsung_grandppltedx

drivers/staging/android/binder.c
@@ -1639,18 +1639,27 @@ static struct binder_ref *binder_get_ref_olocked(struct binder_proc *proc,
 	return NULL;
 }
 
-static void dump_ref_desc_tree(struct binder_ref *new_ref)
+
+static void dump_ref_desc_tree(struct binder_ref *new_ref, struct rb_node *n)
 {
 	struct binder_proc *proc = new_ref->proc;
 	uint32_t desc = new_ref->data.desc;
-	struct rb_node *n, *p;
+	struct rb_node *p;
+	struct binder_ref *ref;
 	int i = 0;
 
-	pr_info("BUG.%d:%d: dump of refs_by_desc rb tree, new desc=%u\n",
-			proc->pid, current->pid, desc);
+	pr_info("BUG.%d:%d: dump of refs_by_desc rb tree, new desc=%u, n%sNULL\n",
+			proc->pid, current->pid, desc, n ? "!=" : "==");
+	if (n) {
+		ref = rb_entry(n, struct binder_ref, rb_node_desc);
+		pr_info("ref containing n: id %d desc %u n %d s %d w %d\n",
+			ref->data.debug_id, ref->data.desc,
+			ref->node->debug_id,
+			ref->data.strong, ref->data.weak);
+	}
+
 	for (n = rb_first(&proc->refs_by_desc); n != NULL; n = rb_next(n)) {
 		struct binder_ref *left=NULL, *right=NULL, *parent=NULL;
-		struct binder_ref *ref;
 
 		ref = rb_entry(n, struct binder_ref, rb_node_desc);
 		if (n->rb_left)
@@ -1743,7 +1752,7 @@ static struct binder_ref *binder_get_ref_for_node_olocked(
 		else if (new_ref->data.desc > ref->data.desc)
 			p = &(*p)->rb_right;
 		else {
-			dump_ref_desc_tree(new_ref);
+			dump_ref_desc_tree(new_ref, n);
 			BUG();
 		}
 	}